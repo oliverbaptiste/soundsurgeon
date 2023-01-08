096e8739724a808958c0a07a11004c1c4cd567da
Oliver Baptiste <github@oliverbaptiste.com>
2023-01-07 21:40:29 -0500

    Minor refactoring: moving header from index.html to App.view, format tweaks

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ import keyboardJS from 'keyboardjs';
 
 $(function(){
   const CONSTANTS = {
-    HOME_ROW: [ 'a', 's', 'd', 'f', 'j', 'k', 'l', 'semicolon' ],
-    DEFAULT_SCALE: [ 'C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5' ],
-    MODES: [ 'ionian', 'dorian', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian' ],
+    HOME_ROW: ['a', 's', 'd', 'f', 'j', 'k', 'l', 'semicolon'],
+    DEFAULT_SCALE: ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'],
+    MODES: ['ionian', 'dorian', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian'],
     NOTE_RANGE: [
-      {'MIN': 'a0'},
-      {'MAX': 'c8'}
+      { 'MIN': 'a0' },
+      { 'MAX': 'c8' }
     ]
   };
   const App = {
@@ -42,7 +42,7 @@ $(function(){
         });
       });
       $('.mute').change(function(e) {
-        if ( $(this).is(':checked') ) {
+        if ($(this).is(':checked')) {
           Tone.Destination.mute = true;
         } else {
           Tone.Destination.mute = false;
@@ -105,179 +105,185 @@ $(function(){
     },
     view: () => {
       const template = `
-      <div class="keyboard">
-        <div class="left hand">
-          <label><span>Do </span><button>A</button></label>
-          <label><span>re </span><button>S</button></label>
-          <label><span>mi </span><button>D</button></label>
-          <label><span>fa </span><button>F</button></label>
-        </div>
-        <div class="right hand">
-          <label><span>sol</span><button>J</button></label>
-          <label><span>la </span><button>K</button></label>
-          <label><span>ti </span><button>L</button></label>
-          <label><span>do </span><button>;</button></label>
-        </div>
-      </div>
-      <div class="controls">
-        <div class="master-vol">
-          <label>
-            <span>Mute</span>
-            <input type="checkbox"
-              class="mute"
-              name="mute">
-          </label>
-          <label>
-            <span>Volume</span>
-            <input type="range"
-              class="volume"
-              name="volume"
-              min="-60"
-              max="0"
-              step="-3">
-          </label>
-        </div>
-        <div class="waveforms">
-          <label>
-            <span>sine</span>
-            <input type="radio" name="wave" value="sine">
-          </label>
-          <label>
-            <span>square</span>
-            <input type="radio" name="wave" value="square">
-          </label>
-          <label>
-            <span>triangle</span>
-            <input type="radio" name="wave" value="triangle">
-          </label>
-          <label>
-            <span>sawtooth</span>
-            <input type="radio" name="wave" value="sawtooth">
-          </label>
-        </div>
-        <div class="envelope">
-          <h2>Envelope</h2>
-          <label>
-            Attack
-            <input
-              type="range"
-              class="attack"
-              name="attack"
-              min="0"
-              max="5"
-              step="0.1">
-          </label>
-          <label>
-            Decay
-            <input
-              type="range"
-              class="decay"
-              name="decay"
-              min="0"
-              max="10"
-              step="0.5">
-          </label>
-          <label>
-            Sustain
-            <input
-              type="range"
-              class="sustain"
-              name="sustain"
-              min="0"
-              max="1"
-              step="0.1">
-          </label>
-          <label>
-            Release
-            <input
-              type="range"
-              class="release"
-              name="release"
-              min="0.5"
-              max="10"
-              step="0.5">
-          </label>
-        </div>
-        <div>
-          <label>
-            <span>Portamento</span>
-            <input
-              type="range"
-              class="portamento"
-              name="portamento"
-              min="0"
-              max="1"
-              value=""
-              step="0.1">
-          </label>
-          <label>
-            <span>Detune</span>
-            <input type="range"
-              class="detune"
-              name="detune"
-              value=""
-              min="0"
-              max="50">
-          </label>
-        </div>
-        <div class="filter">
-          <h2>Filter</h2>
-          <label>
-            <span>Q</span>
-            <input
-              type="range"
-              class="q"
-              name="q"
-              min="0"
-              max="10"
-              step="1">
-          </label>
-          <div class="filter-envelope">
-            <label>
-              <span>Attack</span>
-              <input
-                type="range"
-                class="attack"
-                name="attack"
-                min="0"
-                max="5"
-                step="0.1">
-            </label>
-            <label>
-              <span>Decay</span>
-              <input
-                type="range"
-                class="decay"
-                name="decay"
-                min="0"
-                max="10"
-                step="0.5">
-            </label>
-            <label>
-              <span>Sustain</span>
-              <input
-                type="range"
-                class="sustain"
-                name="sustain"
-                min="0"
-                max="1"
-                step="0.1">
-            </label>
-            <label>
-              <span>Release</span>
-              <input
-                type="range"
-                class="release"
-                name="release"
-                min="0.5"
-                max="10"
-                step="0.5">
-            </label>
+        <header>
+          <h1><strong>Soundsurgeon</strong> JS-11-16 HTML5 Synthesizer</h1>
+        </header>
+        <div class="device">
+          <div class="keyboard">
+            <div class="left hand">
+              <label><span>Do </span><button>A</button></label>
+              <label><span>re </span><button>S</button></label>
+              <label><span>mi </span><button>D</button></label>
+              <label><span>fa </span><button>F</button></label>
+            </div>
+            <div class="right hand">
+              <label><span>sol</span><button>J</button></label>
+              <label><span>la </span><button>K</button></label>
+              <label><span>ti </span><button>L</button></label>
+              <label><span>do </span><button>;</button></label>
+            </div>
+          </div>
+          <div class="controls">
+            <div class="master-vol">
+              <label>
+                <span>Mute</span>
+                <input
+                  type="checkbox"
+                  class="mute"
+                  name="mute">
+              </label>
+              <label>
+                <span>Volume</span>
+                <input type="range"
+                  class="volume"
+                  name="volume"
+                  min="-60"
+                  max="0"
+                  step="-3">
+              </label>
+            </div>
+            <div class="waveforms">
+              <label>
+                <span>sine</span>
+                <input type="radio" name="wave" value="sine">
+              </label>
+              <label>
+                <span>square</span>
+                <input type="radio" name="wave" value="square">
+              </label>
+              <label>
+                <span>triangle</span>
+                <input type="radio" name="wave" value="triangle">
+              </label>
+              <label>
+                <span>sawtooth</span>
+                <input type="radio" name="wave" value="sawtooth">
+              </label>
+            </div>
+            <div class="envelope">
+              <h2>Envelope</h2>
+              <label>
+                Attack
+                <input
+                  type="range"
+                  class="attack"
+                  name="attack"
+                  min="0"
+                  max="5"
+                  step="0.1">
+              </label>
+              <label>
+                Decay
+                <input
+                  type="range"
+                  class="decay"
+                  name="decay"
+                  min="0"
+                  max="10"
+                  step="0.5">
+              </label>
+              <label>
+                Sustain
+                <input
+                  type="range"
+                  class="sustain"
+                  name="sustain"
+                  min="0"
+                  max="1"
+                  step="0.1">
+              </label>
+              <label>
+                Release
+                <input
+                  type="range"
+                  class="release"
+                  name="release"
+                  min="0.5"
+                  max="10"
+                  step="0.5">
+              </label>
+            </div>
+            <div>
+              <label>
+                <span>Portamento</span>
+                <input
+                  type="range"
+                  class="portamento"
+                  name="portamento"
+                  min="0"
+                  max="1"
+                  value=""
+                  step="0.1">
+              </label>
+              <label>
+                <span>Detune</span>
+                <input type="range"
+                  class="detune"
+                  name="detune"
+                  value=""
+                  min="0"
+                  max="50">
+              </label>
+            </div>
+            <div class="filter">
+              <h2>Filter</h2>
+              <label>
+                <span>Q</span>
+                <input
+                  type="range"
+                  class="q"
+                  name="q"
+                  min="0"
+                  max="10"
+                  step="1">
+              </label>
+              <div class="filter-envelope">
+                <label>
+                  <span>Attack</span>
+                  <input
+                    type="range"
+                    class="attack"
+                    name="attack"
+                    min="0"
+                    max="5"
+                    step="0.1">
+                </label>
+                <label>
+                  <span>Decay</span>
+                  <input
+                    type="range"
+                    class="decay"
+                    name="decay"
+                    min="0"
+                    max="10"
+                    step="0.5">
+                </label>
+                <label>
+                  <span>Sustain</span>
+                  <input
+                    type="range"
+                    class="sustain"
+                    name="sustain"
+                    min="0"
+                    max="1"
+                    step="0.1">
+                </label>
+                <label>
+                  <span>Release</span>
+                  <input
+                    type="range"
+                    class="release"
+                    name="release"
+                    min="0.5"
+                    max="10"
+                    step="0.5">
+                </label>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
       `;
-      $('.device').html( () => {
+      $('.app').html( () => {
         return template;
       });
     },

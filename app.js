26697a5b30d9084c404ec619439aa3302f5490c2
Oliver Baptiste <github@oliverbaptiste.com>
2023-11-17 04:39:16 -0500

    2023-W46-5

--- app.js
+++ app.js
@@ -7,9 +7,25 @@ import keyboardJS from 'keyboardjs';
 
 $(function(){
   const CONSTANTS = {
-    HOME_ROW: ['a', 's', 'd', 'f', 'j', 'k', 'l', 'semicolon'],
-    DEFAULT_SCALE: ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'],
-    MODES: ['ionian', 'dorian', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian'],
+    HOME_ROW: [
+      // QWERTY layout home row
+      'a', 's', 'd', 'f', //  left hand
+      'j', 'k', 'l', 'semicolon' // right hand
+    ],
+    DEFAULT_SCALE: [
+      // C Major scale
+      'C4', 'D4', 'E4', 'F4',
+      'G4', 'A4', 'B4', 'C5'
+    ],
+    MODES: [
+      'ionian',
+      'dorian',
+      'phrygian',
+      'lydian',
+      'mixolydian',
+      'aeolian',
+      'locrian'
+    ],
     NOTE_RANGE: [
       { 'MIN': 'a0' },
       { 'MAX': 'c8' }
@@ -24,57 +40,59 @@ $(function(){
         keyboardJS.bind(elem, function(e) {
           e.preventRepeat();
           synth.triggerAttack(App.currentScale[i]);
-          $(keybtns).removeClass('active');
-          $(keybtns[i]).addClass('active');
+          $( keybtns ).removeClass( 'active' );
+          $( keybtns[i] ).addClass( 'active' );
         },
-        function(event) {
-          $(keybtns[i]).removeClass('active');
+        function() {
+          $( keybtns[i] ).removeClass( 'active' );
           synth.triggerRelease();
         });
       });
-      document.querySelectorAll('.keyboard label').forEach(function(elem, i) {
-        elem.addEventListener('mousedown', function(e) {
-          synth.triggerAttack(App.currentScale[i]);
-        });
-        elem.addEventListener('mouseup', function(e) {
-          synth.triggerRelease();
-        });
-      });
-      $('.mute').change(function(e) {
-        if ($(this).is(':checked')) {
+      document.querySelectorAll('.keyboard label').forEach(
+        function(elem, i) {
+          elem.addEventListener('mousedown', function() {
+            synth.triggerAttack(App.currentScale[i]);
+          });
+          elem.addEventListener('mouseup', function() {
+            synth.triggerRelease();
+          });
+        }
+      );
+      $( '.mute' ).on( 'change', function() {
+        if ($( this ).is( ':checked' )) {
           Tone.Destination.mute = true;
         } else {
           Tone.Destination.mute = false;
         }
-      });
-      $('.volume').change(function(e) {
-        Tone.Destination.volume.value = $(this).val();
-      });
-      $('.waveforms input').change(function(e) {
-        var waveform = $(this).val();
+      } );
+      $( '.volume' ).on( 'change', function() {
+        Tone.Destination.volume.value = $( this ).val();
+      } );
+      $( '.waveforms input' ).on( 'change', function() {
+        var waveform = $( this ).val();
         synth.oscillator.type = waveform;
-      });
-      $('.envelope input').change(function(e) {
+      } );
+      $( '.detune' ).on('change', function() {
+        synth.detune.value = $( this ).val();
+      } );
+      $( '.portamento' ).on( 'change', function() {
+        synth.portamento = $( this ).val();
+      } );
+      $( '.envelope input' ).on( 'change', function() {
         var controlName = this.name;
-        var controlValue = $(this).val();
+        var controlValue = $( this ).val();
         var currentEnv = synth.envelope;
-        currentEnv[controlName] = controlValue;
-      });
-      $('.portamento').change(function(e) {
-        synth.portamento = $(this).val();
-      });
-      $('.detune').change(function(e) {
-        synth.detune.value = $(this).val();
-      });
-      $('.filter .q').change(function(e) {
-        synth.filter.Q.value = $(this).val();
-      });
-      $('.filter-envelope input').change(function(e) {
+        currentEnv[ controlName ] = controlValue;
+      } );
+      $( '.filter .q' ).on( 'change', function() {
+        synth.filter.Q.value = $( this ).val();
+      } );
+      $( '.filter-envelope input' ).on ( 'change', function() {
         var controlName = this.name;
-        var controlValue = $(this).val();
+        var controlValue = $( this ).val();
         var currentEnv = synth.filterEnvelope;
-        currentEnv[controlName] = controlValue;
-      });
+        currentEnv[ controlName ] = controlValue;
+      } );
     },
     generateScale: function(tonic, scaleType) {
       var tonicObj = teoria.note(tonic);
@@ -89,18 +107,40 @@ $(function(){
     },
     initControls: function(synth) {
       var initWaveform = 'input[value="' + synth.oscillator.type + '"]';
-      $(initWaveform).attr('checked', 'checked');
-      $('.envelope .attack').val(synth.envelope.attack);
-      $('.envelope .sustain').val(synth.envelope.sustain);
-      $('.envelope .decay').val(synth.envelope.decay);
-      $('.envelope .release').val(synth.envelope.release);
-      $('.portamento').val(synth.portamento);
-      $('.detune').val(synth.detune.value);
-      $('.filter .q').val(synth.filter.Q.value);
-      $('.filter-envelope .attack').val(synth.filterEnvelope.attack);
-      $('.filter-envelope .sustain').val(synth.filterEnvelope.sustain);
-      $('.filter-envelope .decay').val(synth.filterEnvelope.decay);
-      $('.filter-envelope .release').val(synth.filterEnvelope.release);
+      $( initWaveform ).attr( 'checked', 'checked' );
+      $( '.envelope .attack' ).val(
+        synth.envelope.attack
+      );
+      $( '.envelope .sustain' ).val(
+        synth.envelope.sustain
+      );
+      $( '.envelope .decay' ).val(
+        synth.envelope.decay
+      );
+      $( '.envelope .release' ).val(
+        synth.envelope.release
+      );
+      $( '.portamento' ).val(
+        synth.portamento
+      );
+      $( '.detune' ).val(
+        synth.detune.value
+      );
+      $( '.filter .q' ).val(
+        synth.filter.Q.value
+      );
+      $( '.filter-envelope .attack' ).val(
+        synth.filterEnvelope.attack
+      );
+      $( '.filter-envelope .sustain' ).val(
+        synth.filterEnvelope.sustain
+      );
+      $( '.filter-envelope .decay' ).val(
+        synth.filterEnvelope.decay
+      );
+      $( '.filter-envelope .release' ).val(
+        synth.filterEnvelope.release
+      );
     },
     view: () => {
       const html = String.raw;
@@ -142,19 +182,51 @@ $(function(){
             <div class="waveforms">
               <label>
                 <span>sine</span>
-                <input type="radio" name="wave" value="sine">
+                <input type="radio"
+                  name="wave"
+                  value="sine">
               </label>
               <label>
                 <span>square</span>
-                <input type="radio" name="wave" value="square">
+                <input type="radio"
+                  name="wave"
+                  value="square">
               </label>
               <label>
                 <span>triangle</span>
-                <input type="radio" name="wave" value="triangle">
+                <input
+                  type="radio"
+                  name="wave"
+                  value="triangle">
               </label>
               <label>
                 <span>sawtooth</span>
-                <input type="radio" name="wave" value="sawtooth">
+                <input
+                  type="radio"
+                  name="wave"
+                  value="sawtooth">
+              </label>
+            </div>
+            <div>
+              <label>
+                <span>Detune</span>
+                <input type="range"
+                  class="detune"
+                  name="detune"
+                  value=""
+                  min="0"
+                  max="50">
+              </label>
+              <label>
+                <span>Portamento</span>
+                <input
+                  type="range"
+                  class="portamento"
+                  name="portamento"
+                  min="0"
+                  max="1"
+                  value=""
+                  step="0.1">
               </label>
             </div>
             <div class="envelope">
@@ -200,40 +272,8 @@ $(function(){
                   step="0.5">
               </label>
             </div>
-            <div>
-              <label>
-                <span>Portamento</span>
-                <input
-                  type="range"
-                  class="portamento"
-                  name="portamento"
-                  min="0"
-                  max="1"
-                  value=""
-                  step="0.1">
-              </label>
-              <label>
-                <span>Detune</span>
-                <input type="range"
-                  class="detune"
-                  name="detune"
-                  value=""
-                  min="0"
-                  max="50">
-              </label>
-            </div>
             <div class="filter">
               <h2>Filter</h2>
-              <label>
-                <span>Q</span>
-                <input
-                  type="range"
-                  class="q"
-                  name="q"
-                  min="0"
-                  max="10"
-                  step="1">
-              </label>
               <div class="filter-envelope">
                 <label>
                   <span>Attack</span>
@@ -276,18 +316,30 @@ $(function(){
                     step="0.5">
                 </label>
               </div>
+              <label>
+                <span>“Q”</span>
+                <input
+                  type="range"
+                  class="q"
+                  name="q"
+                  min="0"
+                  max="10"
+                  step="1">
+              </label>
             </div>
           </div>
-        </div>
-      `;
-      $('.app').html( () => {
+        </div>`;
+      $( '.app' ).html( () => {
         return template;
       });
     },
     init: function() {
       App.view();
-      App.synthObj = new Tone.MonoSynth().toDestination();
-      App.currentScale = App.generateScale('c2', CONSTANTS.MODES[0]);
+      App.synthObj = new Tone.MonoSynth();
+      App.synthObj.toDestination();
+      App.currentScale = App.generateScale(
+        'c2', CONSTANTS.MODES[0]
+      );
       Tone.Destination.volume.value = -6;
       App.initControls(App.synthObj);
       App.bindEvents(App.synthObj);
